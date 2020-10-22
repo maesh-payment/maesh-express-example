9f0ab17391aa56d3314161748b8539bd7c8cf73b
aditya-67 <adityagannavarapu.67@gmail.com>
2020-10-22 09:49:14 +0530

    Check path

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 let products = [
     {
