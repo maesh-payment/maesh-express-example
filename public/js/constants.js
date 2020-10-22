493b82b39a68371ca6524de355c0cec8697f0c76
aditya-67 <adityagannavarapu.67@gmail.com>
2020-10-22 11:38:20 +0530

    webstore

--- public/js/constants.js
+++ public/js/constants.js
@@ -1,5 +1,5 @@
 // Local
-const request_url = "http://localhost:5000"
+//const request_url = "http://localhost:5000"
 
 // Server
-//const request_url = "https://maesh-express-example.herokuapp.com"
+const request_url = "https://maesh-express-example.herokuapp.com"
